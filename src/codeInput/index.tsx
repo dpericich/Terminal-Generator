f2386bef1a99bc9750b399cd6642d18e1b32ab58
Daniel Pericich <dpericich@gmail.com>
2023-07-07 22:05:45 -0500

    added scaffolding for code input element

---
+++ src/codeInput/index.tsx
@@ -0,0 +1,12 @@
+import React, { ReactElement } from 'react';
+
+const CodeInputElement = (): ReactElement => {
+    return(
+        <div>
+            <h3>Enter Your Code Here:</h3>
+            <textarea name="" id="" cols={30} rows={10}></textarea>
+        </div>
+    )
+};
+
+export default CodeInputElement;
