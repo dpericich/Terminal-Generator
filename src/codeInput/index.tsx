2d2be4ead69e43057df7172e9d44c9e8508ecd6d
Daniel Pericich <dpericich@gmail.com>
2023-07-08 14:27:15 -0500

    added radio inputs for type of input code

--- src/codeInput/index.tsx
+++ src/codeInput/index.tsx
@@ -4,7 +4,13 @@ const CodeInputElement = (): ReactElement => {
     return(
         <div>
             <h3>Enter Your Code Here:</h3>
-            <textarea name="" id="" cols={30} rows={10}></textarea>
+            <div>
+                <input type="radio" id="commandLine" name="codeType" value="CommandLine" defaultChecked={true} />
+                <label htmlFor="commandLine">Command Line</label>
+                <input type="radio" id="code" name="codeType" value="Code" />
+                <label htmlFor="code">Code</label>
+            </div>
+            <textarea name="" id="" cols={90} rows={20}></textarea>
         </div>
     )
 };
