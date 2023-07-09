521961839ef388e2075dd838ea49573c0815787e
Daniel Pericich <dpericich@gmail.com>
2023-07-08 19:48:02 -0500

    added font-awesome dependencies and font-awesome icons to print screen

--- src/terminalOutput/index.tsx
+++ src/terminalOutput/index.tsx
@@ -1,4 +1,6 @@
 import React, { ReactElement } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faWindows} from '@fortawesome/free-brands-svg-icons';
 // Need to import the helper functions for this here
 
 // This will either accept props or get the input from the store
@@ -9,7 +11,7 @@ const TerminalOutputElement = (): ReactElement => {
             <p>The output render will go here</p>
             <br />
             <h3>How to Take a Screenshot</h3>
-            <p>For PC: <span className='text-bold'></span></p>
+            <span>For PC: <span className='text-bold'>Shift + Windows key <FontAwesomeIcon icon={faWindows} /> + S</span></span>
             {/* Add font awesome here */}
             <p>For Mac: <span className='text-bold'>Command + Shift + 4</span></p>
             <p>For Linux: <span className='text-bold'>PrintScreen</span></p>
