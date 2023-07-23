f8d4078161b22873b60b96a6ff4884f48f1f473f
Daniel Pericich <dpericich@gmail.com>
2023-07-22 20:58:19 -0500

    made codeInput component stateful

--- src/codeInput/index.tsx
+++ src/codeInput/index.tsx
@@ -1,16 +1,22 @@
 import React, { ReactElement } from 'react';
 
-const CodeInputElement = (): ReactElement => {
+interface codeInputElementProps {
+    codeInput: string,
+    setCodeInput: any
+}
+
+const CodeInputElement = <codeInput, setCodeInput>(props: codeInputElementProps): ReactElement => {
     return(
         <div>
             <h3>Enter Your Code Here:</h3>
             <div>
                 <input type="radio" id="commandLine" name="codeType" value="CommandLine" defaultChecked={true} />
                 <label htmlFor="commandLine">Command Line</label>
-                <input type="radio" id="code" name="codeType" value="Code" />
+                <input type="radio" id="code" name="codeType" />
                 <label htmlFor="code">Code</label>
             </div>
-            <textarea name="" id="" cols={90} rows={20}></textarea>
+            <textarea name="" id="" cols={90} rows={20} value={props.codeInput} onChange={(e) => {props.setCodeInput(e.target.value)}}></textarea>
+            <div className="button">Generate Code</div>
         </div>
     )
 };
