44d4d87fb933f0b128e753344f25a886c3f14bec
Daniel Pericich <dpericich@gmail.com>
2023-07-24 23:25:36 -0500

    updated props interface for codeInput component

--- src/codeInput/index.tsx
+++ src/codeInput/index.tsx
@@ -2,10 +2,11 @@ import React, { ReactElement } from 'react';
 
 interface codeInputElementProps {
     codeInput: string,
-    setCodeInput: any
+    setCodeInput: any,
+    setCurrentTab: any
 }
 
-const CodeInputElement = <codeInput, setCodeInput>(props: codeInputElementProps): ReactElement => {
+const CodeInputElement = <codeInput, setCodeInput, setCurrentTab>(props: codeInputElementProps): ReactElement => {
     return(
         <div>
             <h3>Enter Your Code Here:</h3>
@@ -13,12 +14,22 @@ const CodeInputElement = <codeInput, setCodeInput>(props: codeInputElementProps)
                 <input type="radio" id="commandLine" name="codeType" value="CommandLine" defaultChecked={true} />
                 <label htmlFor="commandLine">Command Line</label>
                 <input type="radio" id="code" name="codeType" />
-                <label htmlFor="code">Code</label>
+                <label htmlFor="code">Code Editor</label>
             </div>
-            <textarea name="" id="" cols={90} rows={20} value={props.codeInput} onChange={(e) => {props.setCodeInput(e.target.value)}}></textarea>
-            <div className="button">Generate Code</div>
+            <textarea 
+                style={{resize:'none'}} 
+                name="" 
+                id="" 
+                cols={90} 
+                rows={20} 
+                placeholder="Enter your code here..."
+                value={props.codeInput}
+                onChange={(e) => {props.setCodeInput(e.target.value)}}
+            >
+            </textarea>
+            <div className="button" onClick={() => props.setCurrentTab("terminalOutput")}>Generate Code</div>
         </div>
-    )
+    );
 };
 
 export default CodeInputElement;
