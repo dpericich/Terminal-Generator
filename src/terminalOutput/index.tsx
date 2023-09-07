db847ab4a51b42c17e2fcfbc0c4930805bdf372c
Daniel Pericich <dpericich@gmail.com>
2023-09-06 19:39:44 -0500

    Updated TerminalOutputElement styles and prop drilling

--- src/terminalOutput/index.tsx
+++ src/terminalOutput/index.tsx
@@ -1,14 +1,18 @@
 import React, { ReactElement } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faWindows} from '@fortawesome/free-brands-svg-icons';
+import OutputContainer from '../components/OutputContainer';
 // Need to import the helper functions for this here
+interface outputContainerProps {
+    codeInput: string;
+}
 
 // This will either accept props or get the input from the store
-const TerminalOutputElement = (): ReactElement => {
+const TerminalOutputElement = (props: outputContainerProps): ReactElement => {
     return(
         <div>
             <h3>Your Terminal Output</h3>
-            <p>The output render will go here</p>
+            <OutputContainer codeInput={props.codeInput} themeValue={"default"} />
             <br />
             <h3>How to Take a Screenshot</h3>
             <span>For PC: <span className='text-bold'>Shift + Windows key <FontAwesomeIcon icon={faWindows} /> + S</span></span>
