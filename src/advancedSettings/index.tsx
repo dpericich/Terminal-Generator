57da030c53bf924b1bfad72026d574223069c48b
Daniel Pericich <dpericich@gmail.com>
2023-09-09 11:59:31 -0500

    Added onClick actions for theme settings and style wrappers

--- src/advancedSettings/index.tsx
+++ src/advancedSettings/index.tsx
@@ -12,14 +12,22 @@ const AdvancedSettingsElement = ({ setCodeTheme }: AdvancedSettingsElementProps)
                 <h4>Set Theme</h4>
                 <div className='settings-radio-container'>
                     {/* I will need to remove the defaultChecked and instead use the global app state */}
-                    <input type="radio" id="default" name="theme" value="Default" defaultChecked={true} onClick={() => setCodeTheme("default")} />
-                    <label htmlFor="default">Default</label>
-                    <input type="radio" id="darkMode" name="theme" value="Dark Mode" onClick={() => setCodeTheme("dark")} />
-                    <label htmlFor="darkMode">Dark Mode</label>
-                    <input type="radio" id="retro" name="theme" value="Retro" onClick={() => setCodeTheme("retro")} />
-                    <label htmlFor="retro">Retro</label>
-                    <input type="radio" id="pixie" name="theme" value="Pixie" onClick={() => setCodeTheme("pixie")} />
-                    <label htmlFor="pixie">Pixie</label>
+                    <div className="radio-element-container">
+                        <input type="radio" id="default" name="theme" value="Default" defaultChecked={true} onClick={() => setCodeTheme("default")} />
+                        <label htmlFor="default">Default</label>
+                    </div>
+                    <div className="radio-element-container">
+                        <input type="radio" id="darkMode" name="theme" value="Dark Mode" onClick={() => setCodeTheme("dark")} />
+                        <label htmlFor="darkMode">Dark Mode</label>
+                    </div>
+                    <div className="radio-element-container">
+                        <input type="radio" id="retro" name="theme" value="Retro" onClick={() => setCodeTheme("retro")} />
+                        <label htmlFor="retro">Retro</label>
+                    </div>
+                    <div className="radio-element-container">
+                        <input type="radio" id="pixie" name="theme" value="Pixie" onClick={() => setCodeTheme("pixie")} />
+                        <label htmlFor="pixie">Pixie</label>
+                    </div>
                 </div>
             </div>
             {/* What other options do I want for advanced settings? */}
