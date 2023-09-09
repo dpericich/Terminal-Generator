74ee85fc14e3fb3289ff1633c9bbe3bdc2a6aecb
Daniel Pericich <dpericich@gmail.com>
2023-09-09 11:26:24 -0500

    Added onClick handler to update the different theme values

--- src/advancedSettings/index.tsx
+++ src/advancedSettings/index.tsx
@@ -1,6 +1,10 @@
-import React, { ReactElement } from 'react';
+import { ReactElement } from 'react';
 
-const AdvancedSettingsElement = (): ReactElement => {
+interface AdvancedSettingsElementProps {
+    setCodeTheme: Function
+}
+
+const AdvancedSettingsElement = ({ setCodeTheme }: AdvancedSettingsElementProps): ReactElement => {
     return(
         <div className='settings-container'>
             <h3>Advanced Settings</h3>
@@ -8,13 +12,13 @@ const AdvancedSettingsElement = (): ReactElement => {
                 <h4>Set Theme</h4>
                 <div className='settings-radio-container'>
                     {/* I will need to remove the defaultChecked and instead use the global app state */}
-                    <input type="radio" id="default" name="theme" value="Default" defaultChecked={true} />
+                    <input type="radio" id="default" name="theme" value="Default" defaultChecked={true} onClick={() => setCodeTheme("default")} />
                     <label htmlFor="default">Default</label>
-                    <input type="radio" id="darkMode" name="theme" value="Dark Mode" />
+                    <input type="radio" id="darkMode" name="theme" value="Dark Mode" onClick={() => setCodeTheme("dark")} />
                     <label htmlFor="darkMode">Dark Mode</label>
-                    <input type="radio" id="retro" name="theme" value="Retro" />
+                    <input type="radio" id="retro" name="theme" value="Retro" onClick={() => setCodeTheme("retro")} />
                     <label htmlFor="retro">Retro</label>
-                    <input type="radio" id="pixie" name="theme" value="Pixie" />
+                    <input type="radio" id="pixie" name="theme" value="Pixie" onClick={() => setCodeTheme("pixie")} />
                     <label htmlFor="pixie">Pixie</label>
                 </div>
             </div>
