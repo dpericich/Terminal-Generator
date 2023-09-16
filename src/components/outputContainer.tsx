51a9119a87cf05519921f4681a21aeaac41bf233
Daniel Pericich <dpericich@gmail.com>
2023-09-16 18:19:41 -0500

    Updated outputContainer element

--- src/components/outputContainer.tsx
+++ src/components/outputContainer.tsx
@@ -1,3 +1,28 @@
+import React, { ReactElement } from 'react';
+import { formatTerminalInput, formatCodeEditorInput } from '../utils/codeFormatting';
+
+interface OutputContainerProps {
+    codeInput: string;
+    themeValue: string;
+    codeType: string;
+    codeFontFamily: string;
+}
+
+const OutputContainer = ({codeInput, themeValue}: OutputContainerProps): ReactElement => {
+    return(
+        <div className={`output-container ${themeValue}`}>
+            <div className="output-header">
+                <div className="circle close"></div>
+                <div className="circle minimize"></div>
+                <div className="circle open"></div>
+            </div>
+            <div className="output-body">
                 <pre>
                     {codeInput}
                 </pre>
+            </div>
+        </div>
+    );
+};
+
+export default OutputContainer;
