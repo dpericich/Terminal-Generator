4982767456f6178e6be8df0e22c5c96969ff087f
Daniel Pericich <dpericich@gmail.com>
2023-10-08 15:47:18 -0500

    Added appConfig file to store hex code objects for terminal themese

---
+++ src/data/appConfig.js
@@ -0,0 +1,30 @@
+export const themeSettings = {
+    "default": {
+        "terminalPrimary": "black",
+        "terminalSecondary": "white",
+        "close": "red",
+        "minimize": "yellow",
+        "open": "green"
+    },
+    "dark": {
+        "terminalPrimary": "#082c6c",
+        "terminalSecondary": "#bebebe",
+        "close": "#444444",
+        "minimize": "#f4d47c",
+        "open": "#2069e0"
+    },
+    "retro": {
+        "terminalPrimary": "#FFFCC7",
+        "terminalSecondary": "#4DA394",
+        "close": "#D54751",
+        "minimize": "#59322B",
+        "open": "#EF9A48"
+    },
+    "pixie": {
+        "terminalPrimary": "#e5eec5",
+        "terminalSecondary": "#db8b97",
+        "close": "#541e16",
+        "minimize": "#a2d870",
+        "open": "#295128"
+    }
+}
