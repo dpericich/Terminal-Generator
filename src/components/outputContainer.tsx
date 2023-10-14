a44a3eed9b0ea3069836d83d9ed36ced83801fd0
Daniel Pericich <dpericich@gmail.com>
2023-10-14 16:40:48 -0500

    Updated OutputContainer to use font-family and theme context values

--- src/components/outputContainer.tsx
+++ src/components/outputContainer.tsx
@@ -1,22 +1,28 @@
-import React, { ReactElement } from 'react';
-import { formatTerminalInput, formatCodeEditorInput } from '../utils/codeFormatting';
+import { ReactElement, useContext } from 'react';
+import { themeSettings } from '../data/appConfig';
+import { ThemeContext, FontFamilyContext } from '../App'
 
 interface OutputContainerProps {
     codeInput: string;
-    codeType: string;
-}
+};
 
-// This is where I will want to pull in the theme from context
-const OutputContainer = ({codeInput, codeType }: OutputContainerProps): ReactElement => {
+type ThemeSettingsType = typeof themeSettings;
+
+
+const OutputContainer = ({ codeInput }: OutputContainerProps): ReactElement => {
+    const theme = useContext(ThemeContext);
+    const { terminalPrimary, terminalSecondary, close, minimize, open } = themeSettings[theme as keyof ThemeSettingsType];
+    const fontFamily = useContext(FontFamilyContext);
+
     return(
-        <div className={"output-container"}>
-            <div className="output-header">
-                <div className="circle close"></div>
-                <div className="circle minimize"></div>
-                <div className="circle open"></div>
+        <div className="output-container" style={{backgroundColor: terminalPrimary, color: terminalSecondary }}>
+            <div className="output-header" style={{ borderColor: terminalSecondary }}>
+                <div className="circle close" style={{ backgroundColor: close }} ></div>
+                <div className="circle minimize" style={{ backgroundColor: minimize }}></div>
+                <div className="circle open" style={{ backgroundColor: open }}></div>
             </div>
             <div className="output-body">
-                <pre>
+                <pre style={{ fontFamily: fontFamily }}>
                     {codeInput}
                 </pre>
             </div>
